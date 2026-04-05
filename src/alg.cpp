8854a67efb48ae5877e5a6806b6a08dc5b98e12b
zudindaniil8-art <zudindaniil8@gmail.com>
2026-04-05 23:31:32 +0300

    555553

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,4 +1,6 @@
 // Copyright 2021 NNTU-CS
+#include <algorithm>
+
 int countPairs1(int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; ++i) {
@@ -12,14 +14,31 @@ int countPairs1(int *arr, int len, int value) {
 }
 
 int countPairs2(int *arr, int len, int value) {
+  // Создаем копию массива и сортируем её
+  int *sortedArr = new int[len];
+  for (int i = 0; i < len; ++i) {
+    sortedArr[i] = arr[i];
+  }
+  
+  // Сортировка пузырьком (простая, но работает)
+  for (int i = 0; i < len - 1; ++i) {
+    for (int j = 0; j < len - i - 1; ++j) {
+      if (sortedArr[j] > sortedArr[j + 1]) {
+        int temp = sortedArr[j];
+        sortedArr[j] = sortedArr[j + 1];
+        sortedArr[j + 1] = temp;
+      }
+    }
+  }
+  
   int count = 0;
   int left = 0;
   int right = len - 1;
   while (left < right) {
-    int sum = arr[left] + arr[right];
+    int sum = sortedArr[left] + sortedArr[right];
     if (sum == value) {
-      int leftVal = arr[left];
-      int rightVal = arr[right];
+      int leftVal = sortedArr[left];
+      int rightVal = sortedArr[right];
       if (leftVal == rightVal) {
         int n = right - left + 1;
         count += n * (n - 1) / 2;
@@ -27,11 +46,11 @@ int countPairs2(int *arr, int len, int value) {
       }
       int leftCount = 0;
       int rightCount = 0;
-      while (left <= right && arr[left] == leftVal) {
+      while (left <= right && sortedArr[left] == leftVal) {
         ++leftCount;
         ++left;
       }
-      while (left <= right && arr[right] == rightVal) {
+      while (left <= right && sortedArr[right] == rightVal) {
         ++rightCount;
         --right;
       }
@@ -42,26 +61,60 @@ int countPairs2(int *arr, int len, int value) {
       --right;
     }
   }
+  
+  delete[] sortedArr;
   return count;
 }
 
 int countPairs3(int *arr, int len, int value) {
+  // Создаем копию массива и сортируем её
+  int *sortedArr = new int[len];
+  for (int i = 0; i < len; ++i) {
+    sortedArr[i] = arr[i];
+  }
+  
+  // Сортировка пузырьком
+  for (int i = 0; i < len - 1; ++i) {
+    for (int j = 0; j < len - i - 1; ++j) {
+      if (sortedArr[j] > sortedArr[j + 1]) {
+        int temp = sortedArr[j];
+        sortedArr[j] = sortedArr[j + 1];
+        sortedArr[j + 1] = temp;
+      }
+    }
+  }
+  
   int count = 0;
   for (int i = 0; i < len; ++i) {
-    int target = value - arr[i];
+    int target = value - sortedArr[i];
+    // Бинарный поиск с подсчетом всех вхождений
     int left = i + 1;
     int right = len - 1;
+    int firstPos = -1;
+    
+    // Находим первое вхождение target
     while (left <= right) {
       int mid = (left + right) / 2;
-      if (arr[mid] == target) {
-        ++count;
-        break;
-      } else if (arr[mid] < target) {
+      if (sortedArr[mid] == target) {
+        firstPos = mid;
+        right = mid - 1;
+      } else if (sortedArr[mid] < target) {
         left = mid + 1;
       } else {
         right = mid - 1;
       }
     }
+    
+    // Если нашли, считаем количество вхождений
+    if (firstPos != -1) {
+      int lastPos = firstPos;
+      while (lastPos + 1 < len && sortedArr[lastPos + 1] == target) {
+        ++lastPos;
+      }
+      count += (lastPos - firstPos + 1);
+    }
   }
+  
+  delete[] sortedArr;
   return count;
 }
