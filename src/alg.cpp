479c86de6428b881e58f80fa13436ac0a6faf7e3
zudindaniil8-art <zudindaniil8@gmail.com>
2026-04-05 22:52:25 +0300

    44444442

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,79 +1,96 @@
+Понял проблему. Нужно, чтобы все три функции возвращали одинаковое количество уникальных пар. В предыдущих реализациях были ошибки с подсчетом. Вот исправленная версия:
+
+```cpp
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-int count=0;
-for(int i=0;i<len;i++){
-for(int j=i+1;j<len;j++){
-if(arr[i]+arr[j]==value)count++;
-}
-}
-return count;
+  int count = 0;
+  for (int i = 0; i < len; ++i) {
+    for (int j = i + 1; j < len; ++j) {
+      if (arr[i] + arr[j] == value) {
+        ++count;
+      }
+    }
+  }
+  return count;
 }
 
 int countPairs2(int *arr, int len, int value) {
-int count=0;
-int left=0;
-int right=len-1;
-while(left<right){
-int sum=arr[left]+arr[right];
-if(sum==value){
-int leftVal=arr[left];
-int rightVal=arr[right];
-int leftCount=0;
-int rightCount=0;
-while(left<=right&&arr[left]==leftVal){
-leftCount++;
-left++;
-}
-while(left<=right&&arr[right]==rightVal){
-rightCount++;
-right--;
-}
-if(leftVal==rightVal){
-count+=leftCount*(leftCount-1)/2;
-}else{
-count+=leftCount*rightCount;
-}
-}else if(sum<value){
-left++;
-}else{
-right--;
-}
-}
-return count;
-}
-
-int binarySearch(int *arr,int left,int right,int target){
-while(left<=right){
-int mid=left+(right-left)/2;
-if(arr[mid]==target)return mid;
-if(arr[mid]<target)left=mid+1;
-else right=mid-1;
-}
-return -1;
+  int count = 0;
+  int left = 0;
+  int right = len - 1;
+  while (left < right) {
+    int sum = arr[left] + arr[right];
+    if (sum == value) {
+      if (arr[left] == arr[right]) {
+        int n = right - left + 1;
+        count += n * (n - 1) / 2;
+        break;
+      } else {
+        int left_val = arr[left];
+        int right_val = arr[right];
+        int left_count = 0;
+        int right_count = 0;
+        while (left <= right && arr[left] == left_val) {
+          ++left_count;
+          ++left;
+        }
+        while (left <= right && arr[right] == right_val) {
+          ++right_count;
+          --right;
+        }
+        count += left_count * right_count;
+      }
+    } else if (sum < value) {
+      ++left;
+    } else {
+      --right;
+    }
+  }
+  return count;
 }
 
 int countPairs3(int *arr, int len, int value) {
-int count=0;
-for(int i=0;i<len-1;i++){
-int target=value-arr[i];
-if(target<arr[i])continue;
-int index=binarySearch(arr,i+1,len-1,target);
-if(index!=-1){
-int leftCount=1;
-int rightCount=1;
-while(i+leftCount<len&&arr[i+leftCount]==arr[i])leftCount++;
-int j=index;
-while(j-1>i&&arr[j-1]==target){
-j--;
-rightCount++;
+  int count = 0;
+  for (int i = 0; i < len; ++i) {
+    if (i > 0 && arr[i] == arr[i - 1]) {
+      continue;
+    }
+    int target = value - arr[i];
+    int left = i + 1;
+    int right = len - 1;
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+      if (arr[mid] == target) {
+        if (arr[i] == target) {
+          int n = 0;
+          for (int k = i; k < len && arr[k] == target; ++k) {
+            ++n;
+          }
+          count += n * (n - 1) / 2;
+          i = i + n - 1;
+        } else {
+          int left_count = 1;
+          int right_count = 1;
+          int j = mid - 1;
+          while (j > i && arr[j] == target) {
+            ++right_count;
+            --j;
+          }
+          j = mid + 1;
+          while (j < len && arr[j] == target) {
+            ++right_count;
+            ++j;
+          }
+          count += left_count * right_count;
+        }
+        break;
+      } else if (arr[mid] < target) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+  }
+  return count;
 }
-while(index+1<len&&arr[index+1]==target){
-index++;
-rightCount++;
-}
-count+=leftCount*rightCount;
-i+=leftCount-1;
-}
-}
-return count;
-}
+```
