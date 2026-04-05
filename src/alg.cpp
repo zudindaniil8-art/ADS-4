38d71ca630da80725ce40284b1e636a484fbaa7b
zudindaniil8-art <zudindaniil8@gmail.com>
2026-04-05 22:42:16 +0300

    444444

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,79 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+int count=0;
+for(int i=0;i<len;i++){
+for(int j=i+1;j<len;j++){
+if(arr[i]+arr[j]==value)count++;
 }
+}
+return count;
+}
+
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+int count=0;
+int left=0;
+int right=len-1;
+while(left<right){
+int sum=arr[left]+arr[right];
+if(sum==value){
+int leftVal=arr[left];
+int rightVal=arr[right];
+int leftCount=0;
+int rightCount=0;
+while(left<=right&&arr[left]==leftVal){
+leftCount++;
+left++;
 }
+while(left<=right&&arr[right]==rightVal){
+rightCount++;
+right--;
+}
+if(leftVal==rightVal){
+count+=leftCount*(leftCount-1)/2;
+}else{
+count+=leftCount*rightCount;
+}
+}else if(sum<value){
+left++;
+}else{
+right--;
+}
+}
+return count;
+}
+
+int binarySearch(int *arr,int left,int right,int target){
+while(left<=right){
+int mid=left+(right-left)/2;
+if(arr[mid]==target)return mid;
+if(arr[mid]<target)left=mid+1;
+else right=mid-1;
+}
+return -1;
+}
+
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+int count=0;
+for(int i=0;i<len-1;i++){
+int target=value-arr[i];
+if(target<arr[i])continue;
+int index=binarySearch(arr,i+1,len-1,target);
+if(index!=-1){
+int leftCount=1;
+int rightCount=1;
+while(i+leftCount<len&&arr[i+leftCount]==arr[i])leftCount++;
+int j=index;
+while(j-1>i&&arr[j-1]==target){
+j--;
+rightCount++;
+}
+while(index+1<len&&arr[index+1]==target){
+index++;
+rightCount++;
+}
+count+=leftCount*rightCount;
+i+=leftCount-1;
+}
+}
+return count;
 }
